f64b4eb39f3ffc640463edb85fc424f911779778
MARY <marymwihaki123@gmail.com>
2022-06-16 14:15:21 -0500

    updating JS and adding functions

--- js/app.js
+++ js/app.js
@@ -46,44 +46,72 @@ console.log('app js file is connected')
 //}
 
 
+
+//Global Variable
+let userInput;
+let coffeeType;
+let message;
+
 function userName () {
-    let userName = prompt ('What  is your name ?');
-    console.log('username',userName);
-    return document.write(userName);
+  userInput = prompt('What  is your name ?');
+  // console.log('username',userName);
+  return userInput;
 }
 userName();
 
-if (coffeeType === 1) {
- message = 'Latte Macchiato!'
-}else if ( coffeeType === 2){
+
+
+
+
+function coffeeTime(){
+
+  let coffee = prompt ('Choose a number between 1 and 12 to see if I can guess your favorite coffee?');
+  console.log('coffee', typeof coffee);
+
+  coffeeType = parseInt(coffee);
+
+
+  if(coffeeType === 1) {
+    message = 'Latte Macchiato!';
+  }else if ( coffeeType === 2){
     message = 'Black eye!';
-} else if (coffeeType === 3){
+  } else if (coffeeType === 3){
     message = 'Frappe!';
-}else if (coffeeType === 4){ 
+  }else if (coffeeType === 4){
     message = 'Espresso-tonic!';
-}else if (coffeeType === 5){ 
+  }else if (coffeeType === 5){
     message = 'Cappucino';
-}else if(coffeeType === 6) {
+  }else if(coffeeType === 6) {
     message = 'Espresso Fredo';
-}else if(coffeeType === 7) {
+  }else if(coffeeType === 7) {
     message = 'Ra coffee';
-}else if(coffeeType === 8) {
+  }else if(coffeeType === 8) {
     message = 'Iced latte';
-}else if (coffeeType === 9) {
-    message = 'Cold Brew'
-}else if(coffeeType === 10) {
-    message = 'Romano'
+  }else if (coffeeType === 9) {
+    message = 'Cold Brew';
+  }else if(coffeeType === 10) {
+    message = 'Romano';
 
-}else if(coffeeType === 11) {
-    message = 'Irish Coffee'
-}else if(coffeeType === 12) {
-    message = 'Affogato'
+  }else if(coffeeType === 11) {
+    message = 'Irish Coffee';
+  }else if(coffeeType === 12) {
+    message = 'Affogato';
+  }
+  else {
+    message ='Check out our menu to see some suggestions!';
+  }
+
+
+  console.log ('our return message', message);
 }
-else  {
-message ='COFFEE TIME!';
-}
 
 
-console.log ('our return message', message);
+coffeeTime();
 
-document.write('Hiya' + userName + '!'+ 'your favorite type of coffee is '+ coffeeType + ' ' + message);
+
+
+
+
+
+
+document.write('Hi ya ' + userInput + '! Your favorite type of coffee is ' + coffeeType + ' ' + message);
